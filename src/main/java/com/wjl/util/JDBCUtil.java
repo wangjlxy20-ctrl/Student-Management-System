8b5e35cf866ba0c5626719b05f8d6e87ae1050fe
wangjiale <wangjlxy20@gmail.com>
2026-06-19 22:30:26 +0800

    Initial commit

---
+++ src/main/java/com/wjl/util/JDBCUtil.java
@@ -0,0 +1,16 @@
+package com.wjl.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/student_db?useSSL=false&serverTimezone=Asia/Shanghai";
+    private static final String username = "root";
+    private static final String password = "root";
+    public static Connection getConnection() throws SQLException{
+
+        return DriverManager.getConnection(
+                URL,username,password
+        );
+    }
+}
